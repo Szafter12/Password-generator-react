2fd87843725125d13ebeff2705283e3dd630fa90
Jakub <jakubpachut@gmail.com>
2024-09-24 21:10:36 +0200

    fix some bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	purge: {
+		content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+		options: {
+			safelist: ['bg-slate-800', 'bg-pink-500', 'bg-purple-500', 'bg-emerald-500'],
+		},
+	},
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
